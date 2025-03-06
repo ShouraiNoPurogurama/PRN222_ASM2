3f397cc3b87997fca6618cc627f0c983d324d54e
NA <nhatanhtruong20@gmail.com>
2025-03-06 20:48:31 +0700

    Add initial migration and create Coupons table with sample data

---
+++ Discount/Discount.gRPC/Migrations/20250306053431_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Discount.gRPC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductId = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "Amount", "Description", "ProductId" },
+                values: new object[,]
+                {
+                    { "018c1496-d399-4010-a5b2-1c54070e7d44", 150, "IPhone Discount", "3332779a-e455-42ba-ab0f-a0b0014bd5b0" },
+                    { "67dc335b-f900-4c4a-af4c-2ca32214d8a4", 100, "Samsung Discount", "ed1a486a-bda9-4e90-b5f3-97911c2b26d0" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
